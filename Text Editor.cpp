7e44021ba81b09e2bef30e6d14d5c97e031b7b4a
YoussefMahmoudOksha <http://youssef.mahmoud75359@gmail.com/>
2022-05-08 23:26:46 +0200

    Main

---
+++ Text Editor.cpp
@@ -0,0 +1,21 @@
+// FCAI � Programming 1 � 2022 - Assignment 4
+// Program Name: Text Editor .
+// Last Modification Date: 5/8/2022
+// Author1 and ID and Group: Moaz Alaa Eldin Megahed - 20210587
+// Author2 and ID and Group: Mariam Ramadan Abd El-Hameed - 20210381
+// Author3 and ID and Group: Youssef Mahmoud Okasha Mahmoud - 20210486
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+
+int main()
+{
+
+}
+
+
+
