b21beed0aa144fe499d4bb0cf95adea8784b719a
YoussefMahmoudOksha <http://youssef.mahmoud75359@gmail.com/>
2022-05-16 02:25:59 +0200

    Add the .h and .cpp fils

---
+++ TextEditor.h
@@ -0,0 +1,18 @@
+// FCAI � Programming 1 � 2022 - Assignment 4
+// Program Name: Text Editor .
+// Last Modification Date: 5/16/2022
+// Author1 and ID and Group: Moaz Alaa Eldin Megahed - 20210587
+// Author2 and ID and Group: Mariam Ramadan Abd El-Hameed - 20210381
+// Author3 and ID and Group: Youssef Mahmoud Okasha Mahmoud - 20210486
+
+#ifndef UNTITLED15_TEXTEDITOR_H
+#define UNTITLED15_TEXTEDITOR_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+#endif //UNTITLED15_TEXTEDITOR_H
